e8b49760f496d9f1f39d686480a11196d9d18dc4
Sanya Singh <146763385+sanyasingh1362@users.noreply.github.com>
2026-01-10 23:39:31 +0530

    Login Page

---
+++ src/assets/components/CreateRequest.jsx
@@ -0,0 +1,9 @@
+function CreateRequest() {
+  return (
+    <div>
+      <h2>Create Request Page</h2>
+    </div>
+  );
+}
+
+export default CreateRequest;
---
+++ src/assets/components/Login.jsx
@@ -0,0 +1,50 @@
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+
+function Login() {
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+     e.preventDefault();
+  console.log("Submit clicked"); // 👈 ADD THIS
+    
+    try {
+      console.log("Calling login API"); // 👈 ADD
+      const res = await axios.post("http://localhost:8080/login", {
+        emailId,
+        password,
+      });
+
+      localStorage.setItem("token", res.data);
+
+      // ✅ NAVIGATE HERE
+      navigate("/create-request");
+
+    } catch (err) {
+      alert("Login failed");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        placeholder="Email"
+        value={emailId}
+        onChange={(e) => setEmailId(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
